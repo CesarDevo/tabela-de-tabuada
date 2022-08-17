036f375f7266536b52c9d1c02908a762c6c68eed
César <108280943+CesarDevo@users.noreply.github.com>
2022-08-17 08:41:29 -0300

    update script

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-/
+
+function alerte(){ var x = prompt("Entre com um número maior e menor que 11");
+try {
+if (x <= 0) {
+    throw("er1");
+} if ( x >= 11) {
+    throw("er2");
+} if (isNaN(parseInt(x))) {
+      throw ("er3");
+}}
+
+catch(e) {
+    if(e=="er2"){
+        alert("O número deve ser maior que 0");
+    } if (e == "er2") {
+        alert("o número deve ser maior que 11");
+    } if (e == "er3") {
+        alert ("Entre somente com número maior que 0 e menor que 11")
+    }
+}
+}
